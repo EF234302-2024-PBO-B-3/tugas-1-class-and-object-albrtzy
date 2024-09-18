48a1f019438f2b7297d1b53f2fce21ea61073f9e
albrtzy <raihannalbar87@gmail.com>
2024-09-18 23:35:42 +0700

    update

---
+++ src/ClassAndObject/ClassAndObject/TokoSepeda/sepeda.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+
+namespace ClassAndObject.TokoSepeda
+{
+    public class Sepeda
+    {
+        public string Merk { get; set; }
+        public string Tipe { get; set; }
+        public double Berat { get; set; }
+
+        public Sepeda(string merk, string tipe, double berat)
+        {
+            Merk = merk;
+            Tipe = tipe;
+            Berat = berat;
+        }
+
+        public void TampilkanInformasi()
+        {
+            Console.WriteLine($"Merek: {Merk}, Tipe: {Tipe}, Berat: {Berat.ToString(CultureInfo.InvariantCulture)} kg");
+        }
+    }
+}
