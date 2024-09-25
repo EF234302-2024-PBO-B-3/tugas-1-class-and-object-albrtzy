dafc28eb9cedfe68bb822c34ce523a4f035c546e
albrtzy <raihannalbar87@gmail.com>
2024-09-25 20:55:34 +0700

    commit kedua

--- src/ClassAndObject/ClassAndObject/Perpustakaan/buku.cs
+++ src/ClassAndObject/ClassAndObject/Perpustakaan/buku.cs
@@ -1,13 +1,13 @@
-namespace ClassAndObject.Perpustakaan
+namespace ClassAndObject.Perpustakaan 
 {
-    public class Buku
+    public class Buku 
     {
-        public string Judul { get; set; }
-        public string Penulis { get; set; }
-        public int TahunTerbit { get; set; }
+        public string Judul { get; set; } 
+        public string Penulis { get; set; } 
+        public int TahunTerbit { get; set; } 
 
         public Buku(string judul, string penulis, int tahunTerbit)
-        {
+        {   
             Judul = judul;
             Penulis = penulis;
             TahunTerbit = tahunTerbit;
@@ -15,7 +15,7 @@ namespace ClassAndObject.Perpustakaan
 
         public void TampilkanInfo()
         {
-            Console.WriteLine($"Judul: {Judul}, Penulis: {Penulis}, Tahun Terbit: {TahunTerbit}");
+            Console.WriteLine($"Judul: {Judul}, Penulis: {Penulis}, Tahun Terbit: {TahunTerbit}"); 
         }
     }
 }
--- src/ClassAndObject/ClassAndObject/Program.cs
+++ src/ClassAndObject/ClassAndObject/Program.cs
@@ -1,26 +1,33 @@
-﻿namespace ClassAndObject
+﻿using System;
+using ClassAndObject.Perpustakaan;
+using ClassAndObject.Kendaraan;
+using ClassAndObject.TokoLaptop;
+using ClassAndObject.TokoSepeda;
+using ClassAndObject.OperasiMatematika;
+
+namespace ClassAndObject
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            // Buku
-           
+            Buku buku = new Buku("Halo halo bandung", "Megawati", 1999);
+            buku.TampilkanInfo();
 
-            // Kendaraan
+            Mobil mobil = new Mobil("Toyota", "Avanza", 2028);
+            mobil.TampilkanSpesifikasi();
 
-           
+            Laptop laptop = new Laptop("HP", "Intel i2", 16, 1027);
+            laptop.TampilkanSpesifikasi();
 
-            // Laptop
-           
-           
-            // Sepeda
-            
+            Sepeda sepeda = new Sepeda("Racing", "Mountain Bike", 14.5);
+            sepeda.TampilkanInformasi();
 
-            // Kalkulator
-            
-
-
+            Kalkulator kalkulator = new Kalkulator();
+            Console.WriteLine($"Penjumlahan: {kalkulator.Tambah(10, 50)}");
+            Console.WriteLine($"Pengurangan: {kalkulator.Kurang(10, 20)}");
+            Console.WriteLine($"Perkalian: {kalkulator.Kali(10, 5)}");
+            Console.WriteLine($"Pembagian: {kalkulator.Bagi(10, 30)}");
         }
     }
 }
